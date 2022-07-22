68f4f0bd11b740cce931cb1bc0aeb58779892863
Tabizzz <tabatha.reds@gmail.com>
2022-07-22 12:45:06 -0500

    Create a temporal ModType

---
+++ ImGuiLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using HookList = Terraria.ModLoader.Core.HookList<ImGUI.ModImGui>;
+
+namespace ImGUI;
+
+internal class ImGuiLoader
+{
+	internal static readonly List<ModImGui> guis = new();
+	private static readonly List<HookList> hooks = new();
+
+	private static HookList HookDebugGUI = AddHook<Action>(p => p.DebugGUI);
+
+	public static void DebugGUI()
+	{
+		foreach (var gui in HookDebugGUI.Enumerate(guis))
+		{
+			gui.DebugGUI();
+		}
+	}
+
+	internal static void UpdateHooks()
+	{
+		foreach (var hook in hooks)
+		{
+			hook.Update(guis);
+		}
+	}
+
+	private static HookList AddHook<F>(Expression<Func<ModImGui, F>> func) where F : Delegate
+	{
+		var hook = HookList.Create(func);
+
+		hooks.Add(hook);
+
+		return hook;
+	}
+}
---
+++ ModImGui.cs
@@ -0,0 +1,48 @@
+﻿using Terraria.ModLoader;
+
+namespace ImGUI;
+
+
+public abstract class ModImGui : ModType, IIndexed
+{
+	public ushort Index { get; set; }
+
+	public static ushort NextIndex;
+
+	public sealed override void Load()
+	{
+
+	}
+
+	protected sealed override void Register()
+	{
+		ModTypeLookup<ModImGui>.Register(this);
+		Index = NextIndex++;
+		ImGuiLoader.guis.Add(this);
+
+	}
+
+	public sealed override void SetupContent()
+	{
+
+	}
+
+	public sealed override void Unload()
+	{
+		
+	}
+
+	public virtual void DebugGUI()
+	{
+	}
+
+	public virtual void TabGUI()
+	{
+
+	}
+
+	public virtual void CustomGUI()
+	{
+
+	}
+}
