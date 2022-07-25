d875f8f89623954840aaca4fca01e48b6f56ea69
Tabizzz <tabatha.reds@gmail.com>
2022-07-25 13:25:03 -0500

    Added loader with old HookList

---
+++ ImGuiLoader.cs
@@ -0,0 +1,83 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Terraria.ModLoader.Core;
+
+namespace ImGUI;
+
+public class ImGuiLoader
+{
+	internal static readonly List<ModImGui> guis = new();
+	private static readonly List<HookList> hooks = new();
+
+	private class HookList
+	{
+		public int[] arr = new int[0];
+		public readonly MethodInfo method;
+
+		public HookList(MethodInfo method)
+		{
+			this.method = method;
+		}
+	}
+
+	private static HookList HookForeroundDraw = AddHook<Action<ImDrawListPtr>>(p => p.ForeroundDraw);
+
+	public static void ForeroundDraw(ImDrawListPtr drawList)
+	{
+		foreach (var gui in HookForeroundDraw.arr)
+		{
+			guis[gui].ForeroundDraw(drawList);
+		}
+	}
+
+	private static HookList HookBackgroundDraw = AddHook<Action<ImDrawListPtr>>(p => p.BackgroundDraw);
+
+	public static void BackgroundDraw(ImDrawListPtr drawList)
+	{
+		foreach (var gui in HookBackgroundDraw.arr)
+		{
+			guis[gui].BackgroundDraw(drawList);
+		}
+	}
+
+	private static HookList HookDebugGUI = AddHook<Action>(p => p.DebugGUI);
+
+	public static void DebugGUI()
+	{
+		foreach (var gui in HookDebugGUI.arr)
+		{
+			guis[gui].DebugGUI();
+		}
+	}
+
+	private static HookList HookCustomGUI = AddHook<Action>(p => p.CustomGUI);
+
+	public static void CustomGUI()
+	{
+		foreach (var gui in HookCustomGUI.arr)
+		{
+			guis[gui].CustomGUI();
+		}
+	}
+
+	internal static void UpdateHooks()
+	{
+		foreach (var hook in hooks)
+		{
+			hook.arr = guis.WhereMethodIsOverridden(hook.method).Select(p => (int)p.Index).ToArray();
+		}
+	}
+
+	private static HookList AddHook<F>(Expression<Func<ModImGui, F>> func) where F : Delegate
+	{
+		var hook = new HookList(func.ToMethodInfo());
+
+		hooks.Add(hook);
+
+		return hook;
+	}
+}
---
+++ ModImGui.cs
@@ -0,0 +1,67 @@
+﻿using ImGuiNET;
+using Terraria.ModLoader;
+
+namespace ImGUI;
+
+
+public abstract class ModImGui : ModType
+{
+	public ushort Index { get; set; }
+
+	public static ushort NextIndex;
+
+	public sealed override void Load()
+	{
+
+	}
+
+	protected sealed override void Register()
+	{
+		ModTypeLookup<ModImGui>.Register(this);
+		Index = NextIndex++;
+		ImGuiLoader.guis.Add(this);
+
+	}
+
+	public sealed override void SetupContent()
+	{
+		SetStaticDefaults();
+	}
+
+	public sealed override void Unload()
+	{
+
+	}
+
+	/// <summary>
+	/// Use ImGui.** statements that you want to render in the Debug window.
+	/// </summary>
+	public virtual void DebugGUI()
+	{
+
+	}
+
+	/// <summary>
+	/// Create own separate windows.Don't leave ImGui.** instructions on the air, make sure to create a window.
+	/// </summary>
+	public virtual void CustomGUI()
+	{
+
+	}
+
+	/// <summary>
+	/// Use it to draw things behind all ImGUI windows but above terraria, useful for primitives like hitboxes.
+	/// </summary>
+	public virtual void BackgroundDraw(ImDrawListPtr drawList)
+	{
+
+	}
+
+	/// <summary>
+	/// Use it to draw things on top of ImGUI windows.
+	/// </summary>
+	public virtual void ForeroundDraw(ImDrawListPtr drawList)
+	{
+
+	}
+}
