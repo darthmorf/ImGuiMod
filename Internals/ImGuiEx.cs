5ff238a7b7532f2a599f6a8547cdb7756026e70b
Tabizzz <tabatha.reds@gmail.com>
2022-08-20 00:14:31 -0500

    Added ImGuiEx and misc files

---
+++ Internals/ImGuiEx.cs
@@ -0,0 +1,41 @@
+﻿using ImGUI.Internals;
+using System.Text;
+
+namespace ImGuiNET;
+
+public static class ImGuiEx
+{
+	public unsafe static bool BeginTabItem(string label, ImGuiTabItemFlags flags)
+	{
+		int num = 0;
+		byte* ptr;
+		if (label != null)
+		{
+			num = Encoding.UTF8.GetByteCount(label);
+
+			if (num <= Util.StackAllocationSizeLimit)
+			{
+				var stac = stackalloc byte[num + 1];
+				ptr = stac;
+			}
+			else
+			{
+				ptr = Util.Allocate(num + 1);
+			}
+			int utf = Util.GetUtf8(label, ptr, num);
+			ptr[utf] = 0;
+		}
+		else
+		{
+			ptr = null;
+		}
+
+		byte* p_open2 = null;
+		byte num2 = ImGuiNative.igBeginTabItem(ptr, p_open2, flags);
+		if (num > Util.StackAllocationSizeLimit)
+		{
+			Util.Free(ptr);
+		}
+		return num2 != 0;
+	}
+}
---
+++ Internals/Util.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System;
+
+namespace ImGUI.Internals;
+
+public class Util
+{
+	public const int StackAllocationSizeLimit = 2048;
+
+	internal unsafe static byte* Allocate(int byteCount)
+	{
+		return (byte*)Marshal.AllocHGlobal(byteCount);
+	}
+
+	internal unsafe static void Free(byte* ptr)
+	{
+		Marshal.FreeHGlobal((IntPtr)ptr);
+	}
+
+	internal unsafe static int GetUtf8(string s, byte* utf8Bytes, int utf8ByteCount)
+	{
+		fixed (char* ptr = s)
+		{
+			char* chars = ptr;
+			return Encoding.UTF8.GetBytes(chars, s.Length, utf8Bytes, utf8ByteCount);
+		}
+	}
+}
