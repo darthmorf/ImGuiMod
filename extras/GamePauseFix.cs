2aef57a05c6d0d05089ebe00a12f60d98565f490
Tabizzz <tabatha.reds@gmail.com>
2022-08-22 22:03:50 -0500

    Create GamePauseFix.cs

---
+++ extras/GamePauseFix.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ImGUI.extras;
+
+internal class GamePauseFix : ILoadable
+{
+	public void Load(Mod mod)
+	{
+		On.Terraria.Main.CanPauseGame += Main_CanPauseGame;
+	}
+
+	private bool Main_CanPauseGame(On.Terraria.Main.orig_CanPauseGame orig) =>
+		orig() || (Main.netMode == NetmodeID.SinglePlayer && Main.InGameUI.IsVisible && Main.InGameUI.CurrentState?.GetType()?.Name is "UIModConfig" or "UIModConfigList");
+
+	public void Unload()
+	{
+		On.Terraria.Main.CanPauseGame -= Main_CanPauseGame;
+	}
+}
