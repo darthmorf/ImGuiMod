e2769fd8389b0b1d93b98831732dd924acfc334c
Sam Poirier <sampoirier@email.com>
2024-05-11 21:17:30 +0100

    Fix config localisation

--- ImGUIConfig.cs
+++ ImGUIConfig.cs
@@ -7,31 +7,21 @@ internal class ImGUIConfig : ModConfig
 {
 	public override ConfigScope Mode => ConfigScope.ClientSide;
 
-	[Header("ImGui config")]
-	[Label("Debug Window")]
+	[Header("DebugWindow.Header")]
 	[DefaultValue(false)]
-	[Tooltip("Show debug window")]
 	public bool DebugWindow;
 
-	[Label("Terraria mouse")]
 	[DefaultValue(true)]
-	[Tooltip("Use terraria mouse in imgui instead of normal mouse")]
 	public bool TerrariaMouse;
 
-	[Label("Prevent Interaction")]
 	[DefaultValue(true)]
-	[Tooltip("Prevents iterations with the world behind imGUI, for example if you scroll in imgui it doesn't change your selected item")]
 	public bool PreventInteraction;
 
-	[Label("Window Style")]
 	[DefaultValue(ImGuiStyle.Terraria)]
-	[Tooltip("Color Style of imgui gui")]
 	public ImGuiStyle Style;
 
-	[Header("ImGui dev")]
-	[Label("Metrics Window")]
+	[Header("MetricsWindow.Header")]
 	[DefaultValue(false)]
-	[Tooltip("Show metrics window, for developers only")]
 	public bool ShowMetricsWindow;
 
 	public override void OnChanged()
